34939f8d00db7a7491ca079196e2d75a6381a3a8
Jailsonr12 <104799776+Jailsonr12@users.noreply.github.com>
2022-12-03 17:08:35 -0300

    TrabalhoApiMVC

---
+++ SlnRevisaoCSharpMVC/src/Devs2Blu.ProjetosAula.CSharpMVCRevisao/Models/GetListDataCardsDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace Devs2Blu.ProjetosAula.CSharpMVCRevisao.Models {
+    public class GetListDataCardsDTO {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string image_link { get; set; }
+
+        public string price { get; set; }
+    }
+
+}
---
+++ SlnRevisaoCSharpMVC/src/Devs2Blu.ProjetosAula.CSharpMVCRevisao/Services/ServiceAPI.cs
@@ -0,0 +1,66 @@
+﻿using Devs2Blu.ProjetosAula.CSharpMVCRevisao.Models;
+using System;
+
+namespace Devs2Blu.ProjetosAula.CSharpMVCRevisao.Services {
+    public class ServiceAPI {
+        private readonly HttpClient _httpClient;
+
+        public ServiceAPI() {
+            _httpClient = new HttpClient();
+        }
+
+        public async Task<List<GetListDataCardsDTO>> GetListCards() {
+            return await GetList<GetListDataCardsDTO>(URL_API);
+            
+        }
+
+        public async Task<List<GetListDataCardsDTO>> GetCardByName(string name) {
+            var lista = await GetListCards();
+            List<GetListDataCardsDTO> listReturn = new List<GetListDataCardsDTO>() ;
+            foreach(var make in lista) {
+                if(make.name.Contains(name)) {
+                    listReturn.Add(make);
+                }
+            }
+            return listReturn;
+        }
+
+        #region BaseMethods
+
+        public async Task<T> Get<T>(string url) {
+            var objHttp = await GetAsync(url);
+
+            if (!objHttp.IsSuccessStatusCode)
+                return (T)(object)url;
+
+            return await objHttp.Content.ReadFromJsonAsync<T>();
+
+        }
+
+        public async Task<List<T>> GetList<T>(string url) {
+            var listHttp = await GetAsync(url);
+
+            if (!listHttp.IsSuccessStatusCode)
+                return new List<T>();
+
+            return await listHttp.Content.ReadFromJsonAsync<List<T>>();
+
+        }
+
+        public async Task<HttpResponseMessage> GetAsync(string url) {
+            var getRequest = new HttpRequestMessage() {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(url)
+            };
+            return await _httpClient.SendAsync(getRequest);
+        }
+        #endregion
+
+
+        #region CONSTS
+
+        private const string URL_API = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+
+        #endregion
+    }
+}
